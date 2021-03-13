be7ce3bdfa54b9e0742215dd4dd7f2f0e7e1bfeb
ganesh <ganesheili1998@gmail.com>
2021-03-13 16:19:52 +0530

    login and register page

--- server.js
+++ server.js
@@ -10,15 +10,31 @@ const PORT= process.env.PORT || 3000;
 //Assetsget
 app.use(express.static('public'));
 
-app.get('/',(request,response)=>{
-    response.render("home")
-});
- 
 
 //set templete engine
 app.use(expressLayouts);
 app.set('views',path.join(__dirname,'resources/views'));
 app.set('view engine', 'ejs')
+
+
+
+
+app.get('/',(request,response)=>{
+    response.render("home")
+});
+
+app.get('/cart',(request,response)=>{
+    response.render("customers/cart")
+});
+
+app.get('/login',(request,response)=>{
+    response.render("auth/login")
+});
+
+app.get('/register',(request,response)=>{
+    response.render("auth/register")
+});
+
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on ${PORT}`)
